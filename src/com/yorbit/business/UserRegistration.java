0ba820c6152f705087c3835bf83dbe2683066100
pratik <pratikkumar77@gmail.com>
2020-05-10 14:33:29 -0600

    new

--- src/com/yorbit/business/UserRegistration.java
+++ src/com/yorbit/business/UserRegistration.java
@@ -2,8 +2,11 @@ package com.yorbit.business;
 
 import java.util.Map;
 
+import org.testng.Assert;
+
 import com.yorbit.page.HomePage;
 import com.yorbit.page.RegistrationPage;
+import com.yorbit.page.RegistrationSuccessPage;
 
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
@@ -20,13 +23,37 @@ public class UserRegistration {
 		rp.typeInPwd(data.get("pwd"));
 		rp.typeInName(data.get("name"));
 		rp.selectFromLangDD(data.get("lang"));
+		mobiledriver.hideKeyboard();
 		rp.selectAcceptCheckBox();
 		rp.clcikButtonregister();
 		
 	}
 	
-	public static void verifyRegistration(AndroidDriver<?> mobiledriver, Map<String, String> data) {
-		
+	public static void verifyRegistration(AndroidDriver<?> mobiledriver, Map<String, String> data,String key) {
+		Assert.assertEquals(selectMethodForVerification(key, mobiledriver), data.get(key).toLowerCase());
+	}
+	
+	private static String selectMethodForVerification(String key,AndroidDriver<?> mobiledriver) {
+		RegistrationSuccessPage rsp = new RegistrationSuccessPage((AndroidDriver<MobileElement>) mobiledriver);
+		switch (key.toLowerCase()) {
+		case "email": {
+			return rsp.getlblEmail();
+		}
+		case "name": {
+			return rsp.getlblName();
+		}
+		case "lang": {
+			return rsp.getlblLang();
+		}
+		case "pwd": {
+			return rsp.getlblPwd();
+		}
+		case "username": {
+			return rsp.getlbluserName();
+		}
+		default:
+			throw new IllegalArgumentException("Unexpected value: " + key);
+		}
 	}
 	
 
--- src/com/yorbit/tests/NewUserRegistration.java
+++ src/com/yorbit/tests/NewUserRegistration.java
@@ -8,6 +8,7 @@ import org.testng.annotations.Test;
 import com.relevantcodes.extentreports.LogStatus;
 import com.yorbit.business.PermissionController;
 import com.yorbit.business.UserRegistration;
+import com.yorbit.page.RegistrationSuccessPage;
 import com.yorbit.utilities.Common;
 import com.yorbit.utilities.FileReaderUtil;
 
@@ -15,6 +16,7 @@ public class NewUserRegistration extends BaseTest {
 	
 	@Test
 	public void registerNewUserAndVerify() throws Exception {
+		try {
 			logger = extent.startTest("register New User and Verify");
 			Map<String,String> data = new HashMap<String,String>();
 			lauchApp();
@@ -22,7 +24,15 @@ public class NewUserRegistration extends BaseTest {
 			PermissionController.acceptPermission(mobiledriver);
 			Common.dismissAlert(mobiledriver);
 			UserRegistration.startRegistration(mobiledriver,data);
-			
-			//closeAppAndEndSession();
+			UserRegistration.verifyRegistration(mobiledriver, data, "name");
+			UserRegistration.verifyRegistration(mobiledriver, data, "email");
+			UserRegistration.verifyRegistration(mobiledriver, data, "lang");
+			UserRegistration.verifyRegistration(mobiledriver, data, "pwd");
+			UserRegistration.verifyRegistration(mobiledriver, data, "username");
+			closeAppAndEndSession();
+		}catch (Exception e) {
+			closeAppAndEndSession();
+			Assert.fail();
+		}
 	}
 }
